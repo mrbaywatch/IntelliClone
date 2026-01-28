4be285752974fc24d4f43b924affb60e5507f900
Johannes Irgens <johannes@example.com>
2026-01-28 12:20:47 +0100

    fix: simplify home layout for demo (skip workspace loading)

--- apps/intelliclone/app/home/layout.tsx
+++ apps/intelliclone/app/home/layout.tsx
@@ -1,21 +1,11 @@
-import { use } from 'react';
-
-import { UserWorkspaceContextProvider } from '@kit/accounts/components';
-
 import { withI18n } from '~/lib/i18n/with-i18n';
 import { LanguageProvider } from '~/lib/language-context';
 
-import { loadUserWorkspace } from './_lib/load-user-workspace';
-
 function HomeLayout({ children }: React.PropsWithChildren) {
-  const workspace = use(loadUserWorkspace());
-
   return (
-    <UserWorkspaceContextProvider value={workspace}>
-      <LanguageProvider>
-        {children}
-      </LanguageProvider>
-    </UserWorkspaceContextProvider>
+    <LanguageProvider>
+      {children}
+    </LanguageProvider>
   );
 }
 
